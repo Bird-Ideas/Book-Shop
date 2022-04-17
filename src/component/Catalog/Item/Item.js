49985cfa7ef9e9d2970e82540d0df53f7b1dcd37
d3mage <d3magexgod@gmail.com>
2022-04-17 14:14:05 +0300

    complete: catalog

--- src/component/Catalog/Item/Item.js
+++ src/component/Catalog/Item/Item.js
@@ -8,28 +8,24 @@ const Item = (props) => {
     props.switch();
     props.add_to_basket(props.item.id);
   };
-  console.log(props);
   return (
     <>
-      <div className={s.item}>
+      <div
+        className={s.item}
+        onClick={() => {
+          navigate(`/books/${props.item.id}`);
+        }}
+      >
         <div className={s.img_div}>
-          <a href="#">
-            <img className={s.img} src={props.item.img}></img>
-          </a>
+          <img className={s.img} src={props.item.img}></img>
         </div>
         <div className={s.title}>
-          <div
-            onClick={() => {
-              navigate(`/books/${props.item.id}`);
-            }}
-          >
-            {props.item.title}
-          </div>
+          <div>{props.item.title}</div>
         </div>
-        <div className={s.autor}>{props.item.Author.name}</div>
+        <div className={s.autor}>{props.item.author}</div>
         <div className={s.btn_div}>
           <span className={s.price}>
-            <span className={s.price_currency}>{props.item.currency}</span>
+            <span className={s.price_currency}>UAH</span>
             {props.item.price}
           </span>
           <button className={s.btn} onClick={basket_switch}>
--- src/component/Catalog/Item_Single_Page/Item_Single_Page.js
+++ src/component/Catalog/Item_Single_Page/Item_Single_Page.js
@@ -7,15 +7,11 @@ import img from "../../../media/books_img/dsc_00786.jpg";
 const Item_Single_Page = (props) => {
   let { id } = useParams(); 
   const [book, setBook] = useState(""); 
-  const [author, setAuthor] = useState(""); 
-  const [publisher, setPublisher] = useState(""); 
 
   useEffect(() => {
     async function getBook(){
       const result = await axios.get(`http://localhost:3001/books/${id}`);
       setBook(result.data);
-      setAuthor(result.data.Author.name);  
-      setPublisher(result.data.Publisher.name); 
     }
 
     getBook(); 
@@ -30,7 +26,7 @@ const Item_Single_Page = (props) => {
         </div>
         <div className={s.info}>
           <div className={s.title}>{book.title}</div>
-          <div className={s.autor}>{author}</div>
+          <div className={s.autor}>{book.author}</div>
           <div className={s.stats}>
             {/* <div className={s.stats_item}>
               Raiting:<div className={s.stats_item_line}></div>
@@ -38,42 +34,42 @@ const Item_Single_Page = (props) => {
             </div> */}
             <div className={s.stats_item}>
               Publisher:<div className={s.stats_item_line}></div>
-              <div>{publisher}</div>
+              <div>{book.publisher}</div>
             </div>
-            {/* <div className={s.stats_item}>
+            <div className={s.stats_item}>
               Publish Date:<div className={s.stats_item_line}></div>
-              <div>{props.data.publish_date}</div>
+              <div>{book.year}</div>
             </div>
             <div className={s.stats_item}>
               Pages:<div className={s.stats_item_line}></div>
-              <div>{props.data.pages}</div>
+              <div>{book.pages}</div>
             </div>
             <div className={s.stats_item}>
               Language:<div className={s.stats_item_line}></div>
-              <div>{props.data.language}</div>
+              <div>{book.language}</div>
             </div>
             <div className={s.stats_item}>
-              Type:<div className={s.stats_item_line}></div>
-              <div>{props.data.type}</div>
+              Cover:<div className={s.stats_item_line}></div>
+              <div>{book.cover}</div>
             </div>
             <div className={s.stats_item}>
               EAN/UPC:<div className={s.stats_item_line}></div>
-              <div>{props.data.UPC}</div>
-            </div> */}
+              <div>{book.UPC}</div>
+            </div>
           </div>
-          {/* <div className={s.buy_menu}>
+          <div className={s.buy_menu}>
             <div className={s.price_div}>
               <span className={s.price_span}>
                 <span className={s.price_span_currency}>
-                  {props.data.currency}
+                  UAH
                 </span>
-                {props.data.price}
+                {book.price}
               </span>
             </div>
             <div className={s.btn_div}>
               <button>Buy</button>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
       <div className={s.descriptions}>
